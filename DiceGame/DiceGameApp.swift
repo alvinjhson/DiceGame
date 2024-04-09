0cc842e5e4fc58a85ec402987306f811a9a84020
alvinjhson <alvinbest26@gmail.com>
2024-04-09 11:20:21 +0200

    Initial Commit

---
+++ DiceGame/DiceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGameApp.swift
+//  DiceGame
+//
+//  Created by Alvin Johansson on 2024-04-09.
+//
+
+import SwiftUI
+
+@main
+struct DiceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
